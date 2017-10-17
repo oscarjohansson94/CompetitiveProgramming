87877293295404ef467b55d86e03e244f75c1442
Oscar Johansson <1994.oscar.j@gmail.com>
2017-10-17 16:16:37 +0200

    Add solution to problem Quick Estimate

---
+++ Kattis/quickestimate/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int tc = 0;
+    cin >> tc;
+    while(tc--){
+        string in;
+        cin >> in;
+        cout << in.size() << endl;
+    }
+    return 0;
+}
